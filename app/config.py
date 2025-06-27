e30fe6108c629d2acfc4bc7bc38e1520005f428e
Vedant Pawar <vcpawar1603@gmail.com>
2025-06-27 14:34:10 +0530

    Ignore config.py again after pushing dummy version

--- app/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/feedback_system'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'your_secret_key'  
