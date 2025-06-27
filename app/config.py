6a68453e5b2ddaf53e188040d8bfb329ac3dc67c
Vedant Pawar <134604785+vedant-bitbyte@users.noreply.github.com>
2025-06-27 15:18:39 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Vedant 16@localhost/feedback_system'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/feedback_system'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = '00e28f899164f8f1504fbe2d19b38140'  
+    SECRET_KEY = 'your_secret_key'  
